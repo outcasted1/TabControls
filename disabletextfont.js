bdcf9cbe86cfe428b32f3c135f38f058af5c12d9
outcasted1 <135952728+outcasted1@users.noreply.github.com>
2023-08-14 13:29:49 +0530

    1 file modified

--- disabletextfont.js
+++ disabletextfont.js
@@ -18,27 +18,34 @@ $(document).ready(function () {
 });
 //************************************************************* */** */
 $(document).ready(function () {
-  debugger;
-
   var targetElement = $('.customControl');
-
   var styleValue = targetElement.attr('style');
-
   var styleArray = styleValue.split(';').map(function (item) {
     return item.trim();
   });
-
   var colorItem = styleArray.find(function (item) {
     return item.startsWith('color:');
   });
-
   var colorValue = colorItem.split(':')[1].trim();
-
-  // Modify the inline style to set color with !important
   targetElement.attr(
     'style',
     styleValue.replace(colorItem, 'color: ' + colorValue + ' !important')
   );
-
-  console.log('Color value modified:', colorValue);
+});
+/********************************************************** */
+$(document).ready(function () {
+  debugger;
+  var targetElement = $('.customControl');
+  var styleValue = targetElement.attr('style');
+  var styleArray = styleValue.split(';').map(function (item) {
+    return item.trim();
+  });
+  var colorItem = styleArray.find(function (item) {
+    return item.startsWith('color:');
+  });
+  var colorValue = colorItem.split(':')[1].trim();
+  targetElement.attr(
+    'style',
+    styleValue.replace(colorValue, '' + colorValue + ' !important')
+  );
 });
